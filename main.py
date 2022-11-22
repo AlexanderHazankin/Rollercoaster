a5789f6fe5a5bd22fb1f725b6df5538872a2c7c9
Alexander Hazankin <103335228+AlexanderHazankin@users.noreply.github.com>
2022-11-22 09:44:44 +0200

    Update main.py
    Added a loop function to re-request input in case of an incorrect response.
    Adding a discount to visitors from 45 to 55 years old.

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ if height >= 120:
     print("You can ride the Roller coaster! ")
     bill = 0
     age = int(input("What is your age? "))
+    while age > 120:
+        int(input("Wrong age! What is your age? "))
     if age < 12:
         bill += 5
         print("Child tickets are $5. ")
     elif age <= 18:
         bill += 7
         print("Youth tickets are $7. ")
+    elif 45 <= age <= 55:
+        print("Have a free ride on us!. ")
     else:
         print("Adult tickets are $12. ")
         bill += 12
-    photo = input("Do you want a photo taken? Y or N. ")
 
-    if photo.upper() == "Y":
+    photo = input("Do you want a photo taken? Y or N. ")
+    while photo != "Y" and photo != "N":
+        photo = input("Please insert Y or N! ")
+    if photo == "Y":
         print("Photo costs $3. ")
         bill += 3
 
